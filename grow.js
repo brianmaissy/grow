b73041d339614bbc24bfb2f774d3770934418f39
Brian Maissy <brian.maissy@gmail.com>
2013-03-04 16:26:28 +0200

    segments now grow gradually. and added comments to the options and parameters

--- grow.js
+++ grow.js
@@ -13,14 +13,28 @@ if (Meteor.isClient) {
   function drawFractals(){
     // set up the common parameters and options
     var parameters = {
+      // the current growth location of the tree
+      x: 0,
       y: 399.5,
+      // the length of the next branch of the tree
+      size: 0,
+      // the direction in which the tree is growing, in radians
       direction: Math.PI/2,
+      // the current tint of the tree
       tint: 0
     };
     var options = {
+      // the base color of the tree, either 'red', 'green', or 'blue'
+      color: 'red',
+      // the interval at which to increase the tint at each branch
       tintInterval: 32, 
-      animationDelay: 150,
+      // the time interval to delay at each branch
+      branchDelay: 0,
+      // the speed at which line segments should grow, in pixels per second
+      segmentGrowthSpeed: 100,
+      // the angle, in radians, at which each branch should deflect to the side
       spread: Math.PI/3,
+      // the function determining the length of a branch given the length of its parent branch
       shrink: function(n){ return (n<2) ? 0 : .85*Math.pow(n, .92)-1; }
     };
     // continuously run the animation
@@ -50,7 +64,7 @@ if (Meteor.isClient) {
         // clear the screen
         async.apply(clear),
         // wait a little bit before running it again
-        delay(options.animationDelay)
+        delay(options.branchDelay)
       ], callback);
     });
   }
@@ -75,9 +89,8 @@ if (Meteor.isClient) {
       ctx.strokeStyle='#' + tintString + tintString + 'ff';
     }
 
-    // prepare the line segment
-    ctx.beginPath();
-    ctx.moveTo(parameters.x, parameters.y);
+    var startX = parameters.x;
+    var startY = parameters.y;
 
     // generate the next set of parameters
     parameters = modify(parameters, {
@@ -87,20 +100,43 @@ if (Meteor.isClient) {
       tint: parameters.tint + options.tintInterval
     });
 
-    // draw the line segment
-    ctx.lineTo(parameters.x, parameters.y);
-    ctx.stroke();
+    // gradually draw the line segment
+    growSegment(startX, startY, parameters.x, parameters.y, options.segmentGrowthSpeed, function(){
+      // set the timeout for the next iterations
+      setTimeout(function(){
+        // only pass the callback to one child
+        fractal(modify(parameters, {
+          direction: parameters.direction + options.spread,
+        }), options, callback);
+        fractal(modify(parameters, {
+          direction: parameters.direction - options.spread
+        }), options);
+      }, options.branchDelay);
+    });
+  }
 
-    // set the timeout for the next iterations
-    setTimeout(function(){
-      // only pass the callback to one child
-      fractal(modify(parameters, {
-        direction: parameters.direction + options.spread,
-      }), options, callback);
-      fractal(modify(parameters, {
-        direction: parameters.direction - options.spread
-      }), options);
-    }, options.animationDelay);
+  // recursively animates the growth of a line segment
+  function growSegment(startX, startY, endX, endY, speed, callback){
+    // calculate the distance to grow
+    var distance = Math.sqrt(Math.pow(endX-startX, 2)+Math.pow(endY-startY, 2));
+    if(distance<1){
+      // if we're already there, finish
+      callback();
+    }else{
+      // otherwise, prepare the line segment
+      ctx.beginPath();
+      ctx.moveTo(startX, startY);
+      // calculate the position of the first step
+      var x = startX + (endX-startX)/distance;
+      var y = startY + (endY-startY)/distance;
+      // draw the line segment
+      ctx.lineTo(x, y);
+      ctx.stroke();
+      setTimeout(function(){
+        // draw the remainder of the segment
+        growSegment(x, y, endX, endY, speed, callback); 
+      }, 1000/speed);
+    }
   }
 
   // ========== HELPER FUNCTIONS ==========
