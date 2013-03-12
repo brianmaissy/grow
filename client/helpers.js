a9fefeecbeff219c0d31cd5324df20817b1fbe09
Brian Maissy <brian.maissy@gmail.com>
2013-03-12 02:31:58 +0200

    new math helpers

--- client/helpers.js
+++ client/helpers.js
@@ -23,12 +23,40 @@ function clear(canvas, callback){
 
 // ========== MATH HELPER FUNCTIONS ==========
 
+// rotates a vector by an angle
 function rotate(vector, theta){
   var x = vector[0], y = vector[1];
   return [
     x*Math.cos(theta) - y*Math.sin(theta),
     x*Math.sin(theta) + y*Math.cos(theta)
   ];
+}
+
+// normalizes an angle to between 0 and 2PI
+function normalize(n){
+  if(n < 0) return normalize(n + 2*Math.PI);
+  else if(n > 2*Math.PI) return normalize(n - 2*Math.PI);
+  else return n;
+}
+
+// calculates the direction between two points
+function direction(startX, startY, endX, endY){
+  var deltaX = endX-startX;
+  var deltaY = startY-endY;
+  if(deltaX === 0) return deltaY > 0 ? Math.PI/2 : 3*Math.PI/2;
+  if(deltaY === 0) return deltaX > 0 ? 0 : Math.PI;
+  var atan = Math.atan(deltaY/deltaX);
+  if(atan >= 0){
+    return deltaY > 0 ? atan : atan + Math.PI;
+  }else{
+    return deltaY > 0 ? atan + Math.PI : atan + 2*Math.PI;
+  }
+}
+
+// randomly tweaks a number n up to spread in either direction
+function randomize(n, spread){
+  if(typeof spread == 'undefined') spread = n;
+  return n + 2*spread*(Math.random()-.5);
 }
 
 // ========== OBJECT HELPER FUNCTIONS ==========
